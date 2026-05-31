7e6e679cc4fff80d69aed7dc97419d67bfc1e3d7
ruansbueno <ruansbueno@gmail.com>
2026-05-31 17:55:01 -0300

    configurando banco de dados e criando estrutura de api

--- backend_php/routes/web.php
+++ backend_php/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/run-migrate', function () {
+    Artisan::call('migrate');
+    return Artisan::output();
+});
+
+Route::get('/install-api', function () {
+    Artisan::call('install:api', ['--passport' => true]);
+    return Artisan::output();
+});
